e3ee28a1bbbe7c5d51a7d482cae0782fe7850b8d
Puchong Intusmit <eternaltot@gmail.com>
2018-03-05 01:54:19 +0700

    add read and write data

---
+++ server/app.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const app = express();
+var port = 4000;
+
+var mongojs = require("mongojs");
+var Promise = require("promise");
+var db = mongojs("waterflowdb");
+
+
+app.get('/',function(req,res){
+  res.send("IoT Protocol ready at port :"+port);
+});
+
+app.get('/write/:data',function(req,res){
+  var strParseWriteReq = JSON.stringfy(req.params);
+  var strWriteReq = JSON.parse(strParseWriteReq);
+  writedata(data,res);
+});
+
+app.get('/read/:datasize',function(){
+  var strParseReadReq = JSON.stringfy(req.params);
+  var strReadReq = JSON.parse(strParseReadReq);
+  datasize = strReadReq.datasize;
+  readdata(datasize,res);
+});
+
+
+app.listen(port,function(){
+  var nodeStartTime = new Date();
+  console.log("IoT protocol running on port " + port + " start at " + nodeStartTime);
+});
+
+
+async function writedata(_data,res){
+  await writeDataToDB(_data,res);
+}
+
+function writeDataToDB(_savedata,res){
+  return new Promise(function(resolve,reject){
+    var writecollection = db.collection('waterflowcol');
+    writecollection.insert({
+      recordTime:new Date().getTime(),
+      data:Number(_savedata),
+    },function(err){
+      if(err){
+        console.log(err);
+        res.send(String(err));
+      }else {
+        console.log('record data ok');
+        res.send('record data ok');
+      }
+    });
+  });
+}
+
+async function readdata(_datasize,res){
+  await readDataFromDB(_datasize,res);
+}
+
+function readDataFromDB(_datasize,res){
+  return new Promise(function(resolve,reject){
+    var readcollection = db.collection('waterflowcol');
+    readcollection.find({}).limit(Number(_datasize)).sort({recordTime:-1},function(err,docs){
+      res.jsonp(docs);
+    });
+  });
+}
